14a2e8acdb1c466e0e14a80557d317471cc994f2
Nyiro Gergo <gergo.nyiro@gmail.com>
2017-01-22 15:57:47 +0100

    ext3

--- lec02.hs
+++ lec02.hs
@@ -45,8 +45,8 @@ drawTile Storage = storage
 drawTile Box     = box
 drawTile Blank   = blank
 
-pictureOfMaze :: Picture
-pictureOfMaze = draw21times (\r -> draw21times (\c -> drawTileAt r c))
+pictureOfMaze :: (Coord -> Tile) -> Picture
+pictureOfMaze tile = draw21times (\r -> draw21times (\c -> drawTileAt tile (C r c)))
 
 draw21times :: (Integer -> Picture) -> Picture
 draw21times something = go (-10)
@@ -55,27 +55,42 @@ draw21times something = go (-10)
     go 11 = blank
     go n  = something n & go (n+1)
 
-drawTileAt :: Integer -> Integer -> Picture
-drawTileAt r c = translated (fromIntegral r) (fromIntegral c) (drawTile (maze (C r c)))
+drawTileAt :: (Coord -> Tile) -> Coord ->Picture
+drawTileAt tile coord@(C row col) = translated (fromIntegral row) (fromIntegral col) (drawTile (tile coord))
          
-maze :: Coord -> Tile 
-maze (C x y)
+maze1 :: Coord -> Tile 
+maze1 (C x y)
   | abs x > 4  || abs y > 4  = Blank
   | abs x == 4 || abs y == 4 = Wall
   | x ==  2 && y <= 0        = Wall
   | x ==  3 && y <= 0        = Storage
   | x >= -2 && y == 0        = Box
   | otherwise                = Ground
+         
+maze2 :: Coord -> Tile 
+maze2 (C x y)
+  | abs x > 4  || abs y > 4     = Blank
+  | abs x == 4 || abs y == 4    = Wall
+  | x == -1 && y <= -2          = Wall
+  | x ==  1 && y <= -2          = Wall
+  | x ==  0 && y <= -2          = Storage
+  | x >= -2 && x <= 2 && y == 0 = Box
+  | otherwise                   = Ground
 
 data Direction = R | U | L | D
 
 data Coord = C Integer Integer
 
-data State = S Coord Direction
+data Level = First | Second
 
+data State = S Coord Direction Level
+
+selectMaze :: Level -> (Coord -> Tile)
+selectMaze First  = maze1
+selectMaze Second = maze2
 
 initialState :: State
-initialState = S (C 0 1) R
+initialState = S (C 0 1) R First
 
 atCoord :: Coord -> Picture -> Picture
 atCoord (C x y) pic = translated (fromIntegral x) (fromIntegral y) pic
@@ -87,24 +102,36 @@ adjacentCoord (C x y) L = C (x-1) y
 adjacentCoord (C x y) D = C  x   (y-1)
 
 handleTime :: Double -> State -> State
-handleTime _ c = c
+handleTime _ s = s
 
 handleEvent :: Event -> State -> State
-handleEvent (KeyPress "Esc")   _       = initialState
-handleEvent (KeyPress "Right") (S c _) = S (nextCoord c R) R
-handleEvent (KeyPress "Up")    (S c _) = S (nextCoord c U) U
-handleEvent (KeyPress "Left")  (S c _) = S (nextCoord c L) L
-handleEvent (KeyPress "Down")  (S c _) = S (nextCoord c D) D
+handleEvent (KeyPress "Esc") _ = initialState
+handleEvent (KeyPress "1")   _ = S (C 0 1) R First
+handleEvent (KeyPress "2")   _ = S (C 0 1) R Second
+
+handleEvent (KeyPress key) (S c _ l)
+  = let d = dir key
+        s = S c d l
+    in S (nextCoord s) d l
+  where
+    dir :: Text -> Direction
+    dir "Right"  = R
+    dir "Up"     = U
+    dir "Left"   = L
+    dir "Down"   = D
+    dir _        = R
+
+    nextCoord :: State -> Coord
+    nextCoord (S c d l)
+      = let next = adjacentCoord c d
+            maze = selectMaze l
+            tile = maze next
+        in if tile `elem` [Ground, Storage] then next else c
+
 handleEvent _                  s       = s
 
-nextCoord :: Coord -> Direction -> Coord
-nextCoord c d 
-  = let next = adjacentCoord c d
-    in if (maze next) `elem` [Ground, Storage] then next else c
-
-
 drawState :: State -> Picture
-drawState (S c d) = atCoord c (player d) & pictureOfMaze
+drawState (S c d l) = atCoord c (player d) & (pictureOfMaze (selectMaze l))
 
 main :: IO ()
 main = interactionOf initialState handleTime handleEvent drawState
